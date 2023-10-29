14e3f03ea629537c55665c41ba32708f9478d514
Jason <aquasarus@users.noreply.github.com>
2023-10-29 13:08:22 -0400

    Format usings/line width

--- LifeformEditor.xaml.cs
+++ LifeformEditor.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using Starfield_Interactive_Smart_Slate.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Resources;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Starfield_Interactive_Smart_Slate
 {
