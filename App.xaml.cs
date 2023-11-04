1e17b6c58bf3017b54dee199affe5a055236c9f7
Jason <aquasarus@users.noreply.github.com>
2023-11-03 23:33:56 -0400

    Log unhandled errors for reporting purposes

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,20 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
 
 namespace Starfield_Interactive_Smart_Slate
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            // add crash logging
+            DispatcherUnhandledException += (sender, e) =>
+            {
+                HandleException(e.Exception);
+            };
+
+            // set up database and upgrade if needed
             DatabaseInitializer.InitializeDatabaseFile();
             var currentDatabaseVersion = DatabaseInitializer.CheckVersion();
             while (currentDatabaseVersion < DatabaseInitializer.TargetDatabaseVersion)
@@ -29,6 +35,23 @@ namespace Starfield_Interactive_Smart_Slate
             DatabaseInitializer.SetVersionToLatest();
 
             base.OnStartup(e);
+        }
+        private static void HandleException(Exception ex)
+        {
+            string logFileDirectory = DatabaseInitializer.UserDatabaseFolder();
+
+            if (!Directory.Exists(logFileDirectory))
+            {
+                Directory.CreateDirectory(logFileDirectory);
+            }
+
+            string logFilePath = Path.Combine(logFileDirectory, $"error_{DateTime.Now:yyyy_MM_dd_HHmmss}.log");
+            string errorMessage = $"[{DateTime.Now}] Exception: {ex.Message}\nStack Trace: {ex.StackTrace}\n\n";
+            File.AppendAllText(logFilePath, errorMessage);
+
+            MessageBox.Show($"An unexpected error occured!\n\n" +
+                $"The application will crash. Please find an error log in {logFileDirectory} and report it to my GitHub repo.",
+                "ERROR");
         }
     }
 }
